6ff6f946cab0831562b2a26ee758fc0488b0d03e
juan diego mesa <juandyyy@hotmail.com>
2021-12-26 17:08:48 -0500

    c

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ const iconMenu = document.getElementById("icon-menu");
 
 const mainMenu = document.getElementById("main-menu");
 
-iconMenu.addEventListener('click', ()=>mainMenu.classList.toggle('menu-show'));
+
+
+iconMenu.addEventListener('click',()=>mainMenu.classList.toggle('menu-show'));
+
+
+
+
+
+
+
